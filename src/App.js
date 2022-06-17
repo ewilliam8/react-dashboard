0a3cf69ba9849afaf7f482cc19bae98adee0adb6
Ilya Rogachev <rogachev.ilya@list.ru>
2022-06-17 13:28:13 +0700

    UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
 
 const App = () => {
-    let [likes, setLikes] = useState(0);
-    let [value, setValue] = useState('Text in input');
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'Description 1'},
+        {id: 2, title: 'JavaScript 2', body: 'Description 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Description 3'},
+        {id: 4, title: 'JavaScript 4', body: 'Description 4'},
+        {id: 5, title: 'JavaScript 5', body: 'Description 5'},
+    ]);
 
-    function increment () {
-        setLikes(likes + 1)
+    const [selectedSort, setSelectedSort] = useState('');
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
     }
-    function decrement () {
-        setLikes(likes - 1)
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
     }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a , b) => a[sort].localeCompare(b[sort])));
+    }
+
     return (
-        <div>App
-            <h1>{likes}</h1>
-            <h1>{value}</h1>
-            <input 
-                type="text" 
-                value={value}
-                onChange={event => setValue(event.target.value)}
-                ></input>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+        <div className='App'>
+            <PostForm create={createPost}/>
+            <hr style={{margin: '15px 0'}} />
+            <div>
+                <MyInput
+                    style={{width: '175px'}}
+                    placeholder="Поиск ..."
+                />
+                <br/>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue={"Сортировка по"}
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'},
+                    ]}
+                />
+            </div>
+            {posts.length !== 0
+                ? <PostList remove={removePost} posts={posts} title={'Список 1'}/>
+                : <h1 style={{textAlign: 'center'}}>Посты не были найдены</h1>
+            }
         </div>
-    )
+    );
 }
 
 export default App
